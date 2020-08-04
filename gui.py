25385c2fc14b56b039cf490b4bc0ab712f3ccccb
Krzysztof Szpieg <kszpieg@gmail.com>
2020-08-04 22:39:22 +0200

    Added "prev" and "next image" buttons under photo with functionality. Created buttons "Make tag" and "Save tags" for the future. Added label with image filename under photo.

--- gui.py
+++ gui.py
@@ -26,11 +26,12 @@ class AppPanel(wx.Panel):
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
         left_sizer = wx.BoxSizer(wx.VERTICAL)
         btn_main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        btn_image_sizer = wx.BoxSizer(wx.HORIZONTAL)
         right_sizer = wx.BoxSizer(wx.VERTICAL)
         self.row_obj_dict = {}
-        self.list_of_images = []
-        self.current_person = 0
-        self.total_persons = 0
+        self.file_names = []
+        self.selection = 0
+        self.label_photo = ""
 
         self.list_ctrl = wx.ListCtrl(
             self, size=(650, 150),
@@ -40,7 +41,6 @@ class AppPanel(wx.Panel):
         self.list_ctrl.InsertColumn(1, "File extension", width=100)
         left_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)
         btn_data = [("Select image", btn_main_sizer, self.select_photo),
-                    ("Tag person on this photo", btn_main_sizer, self.tag_persons),
                     ("Generate album", btn_main_sizer, self.open_generator_window)]
         for data in btn_data:
             label, sizer, handler = data
@@ -48,8 +48,21 @@ class AppPanel(wx.Panel):
         left_sizer.Add(btn_main_sizer, 0, wx.CENTER)
 
         bmp_image = wx.Image(wx.EXPAND, wx.EXPAND)
-        self.image_ctrl2 = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(bmp_image))
-        right_sizer.Add(self.image_ctrl2, 0, wx.ALL | wx.ALIGN_LEFT, 5)
+        self.image_ctrl = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(bmp_image))
+        right_sizer.Add(self.image_ctrl, 0, wx.ALL | wx.ALIGN_LEFT, 5)
+
+        self.image_label = wx.StaticText(self, label="")
+        right_sizer.Add(self.image_label, 0, wx.ALL | wx.CENTER, 5)
+
+        btn_data_under_photo = [("Previous image", btn_image_sizer, self.previous_image),
+                    ("Make tag", btn_image_sizer, self.tag_persons),
+                    ("Save tags", btn_image_sizer, self.save_tags_on_the_photo),
+                    ("Next image", btn_image_sizer, self.next_image)]
+        for data in btn_data_under_photo:
+            label, sizer, handler = data
+            self.btn_builder(label, sizer, handler)
+        right_sizer.Add(btn_image_sizer, 0, wx.CENTER)
+
 
         main_sizer.Add(left_sizer, wx.ALIGN_LEFT, 5)
         main_sizer.Add(right_sizer, wx.ALIGN_RIGHT, 5)
@@ -61,18 +74,44 @@ class AppPanel(wx.Panel):
         sizer.Add(btn, 0, wx.ALL | wx.CENTER, 5)
 
     def select_photo(self, event):
-        selection = self.list_ctrl.GetFocusedItem()
-        if selection >= 0:
-            photo = self.row_obj_dict[selection]
+        self.selection = self.list_ctrl.GetFocusedItem()
+        if self.selection >= 0:
+            photo = self.row_obj_dict[self.selection]
             #result_image, detected_objects = recognize_persons(photo)
             #self.list_of_images = crop_objects(photo, detected_objects)
             converted_image = wxBitmapFromCvImage(photo)
             bitmap = optimize_bitmap_person(wx.Bitmap(converted_image))
-            self.image_ctrl2.SetBitmap(bitmap)
+            self.image_ctrl.SetBitmap(bitmap)
+            self.image_label.SetLabelText(self.file_names[self.selection])
             self.Refresh()
             self.Layout()
 
+    def previous_image(self, event):
+        if self.selection == 0:
+            self.selection = len(self.row_obj_dict) - 1
+        else:
+            self.selection -= 1
+        photo = self.row_obj_dict[self.selection]
+        converted_image = wxBitmapFromCvImage(photo)
+        bitmap = optimize_bitmap_person(wx.Bitmap(converted_image))
+        self.image_ctrl.SetBitmap(bitmap)
+        self.image_label.SetLabelText(self.file_names[self.selection])
+
+    def next_image(self, event):
+        if self.selection == (len(self.row_obj_dict) - 1):
+            self.selection = 0
+        else:
+            self.selection += 1
+        photo = self.row_obj_dict[self.selection]
+        converted_image = wxBitmapFromCvImage(photo)
+        bitmap = optimize_bitmap_person(wx.Bitmap(converted_image))
+        self.image_ctrl.SetBitmap(bitmap)
+        self.image_label.SetLabelText(self.file_names[self.selection])
+
     def tag_persons(self, event):
+        print("Not implemented")
+
+    def save_tags_on_the_photo(self, event):
         print("Not implemented")
 
     def open_generator_window(self):
@@ -82,6 +121,7 @@ class AppPanel(wx.Panel):
     def update_files_listing(self, folder_path):
         self.current_folder_path = folder_path
         self.list_ctrl.ClearAll()
+        self.file_names.clear()
 
         self.list_ctrl.InsertColumn(0, "File name", width=250)
         self.list_ctrl.InsertColumn(1, "Date", width=150)
@@ -94,6 +134,7 @@ class AppPanel(wx.Panel):
         for photo in photos:
             photo_object = cv2.imread(photo)
             self.list_ctrl.InsertItem(index, Path(photo).stem)
+            self.file_names.append(Path(photo).stem + Path(photo).suffix)
             self.list_ctrl.SetItem(index, 1, str(time.strftime('%d/%m/%Y', time.gmtime(os.path.getmtime(photo)))))
             self.list_ctrl.SetItem(index, 2, Path(photo).suffix)
             self.list_ctrl.SetItem(index, 3, str(os.path.getsize(photo)) + " B")
